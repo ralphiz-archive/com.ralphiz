e96a455df8b344646b3b5f8b1a51fd1e4e23d2f3
Ralph Cacho <cacho.rc@gmail.com>
2016-05-20 09:42:15 -0700

    Install Debugbar

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Additional Service Providers...
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +206,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Additional Facades...
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
